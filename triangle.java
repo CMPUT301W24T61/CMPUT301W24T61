53a8db3a90df4fa1f3bd92d2df0e5e9efcb72ab8
deol1017 <hsdeol@ualberta.ca>
2024-02-03 13:15:24 -0700

    Added a triangle class that extends shape

---
+++ triangle.java
@@ -0,0 +1,16 @@
+public class Triangle extends Shape {
+	private double height;
+	
+	public Circle(double height) {
+		this.height = height;
+	}
+	
+	private double getHeight() {
+		return height;
+	}
+
+	private double setHeight(double height) {
+		this.height = height;
+	}
+
+	}
